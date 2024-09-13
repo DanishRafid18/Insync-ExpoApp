f0db818a9b0f7f8e6587ed87bc17336ad3215096
DanishRafid18 <danishrafid18@gmail.com>
2024-09-13 10:18:01 +1000

    added logo and text at start screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    
+    Inter: require('../assets/fonts/Inter_28pt-Light.ttf')
   });
 
   useEffect(() => {
